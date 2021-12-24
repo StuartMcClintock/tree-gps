3d4e143706239a5706f3c7e108e4863471e61b6a
Stuart McClintock <stuart.e.mcc@gmail.com>
2021-12-23 18:06:35 -0800

    add remove button

--- src/components/locationCollector.js
+++ src/components/locationCollector.js
@@ -9,31 +9,38 @@ export default class LocationCollector extends Component {
     };
   }
 
-  componentDidMount() {
+  /*componentDidMount() {
     navigator.geolocation.getCurrentPosition(function(position) {
       console.log("Latitude is :", position.coords.latitude);
       console.log("Longitude is :", position.coords.longitude);
     });
-  }
+  }*/
+
   headers = [
     { label: "Latitude", key: "latitude" },
     { label: "Longitude", key: "longitude" },
   ];
 
-  updateCoords = () => {
+  addCoord = () => {
     navigator.geolocation.getCurrentPosition((position) => {
-      const newList = this.state.data.concat({ latitude: position.coords.latitude, longitude: position.coords.longitude});
+      const newList = this.state.data.unshift({ latitude: position.coords.latitude, longitude: position.coords.longitude}); // concat => unshift
       this.setState({data: newList})
     });
+  }
+
+  removeCoord = () => {
+    this.setState({data: data.splice(1)})
   }
 
   render() {
     return (
       <div>
-        <h2>GeoLocation</h2>
-        <button onClick={() => this.updateCoords()}>Add Location</button>
+        <h2>TreeGPS</h2>
+        <button onClick={() => this.addCoord()}>Add Current Location</button>
+        <button onClick={() => this.removeCoord()}>Remove Latest Location</button>
         <br />
         {this.state.data.map(data => <p>Your latitude is: {data.latitude}, longitude: {data.longitude}</p>)}
+        <br />
         <CSVLink data={this.state.data} headers={this.headers}>
           Download CSV
         </CSVLink>

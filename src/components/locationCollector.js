81d609fe6bd73b3ba95c722a210b583a0c65b3da
Stuart McClintock <stuart.e.mcc@gmail.com>
2021-12-23 17:46:42 -0800

    restoring csv addition

--- src/components/locationCollector.js
+++ src/components/locationCollector.js
@@ -1,16 +1,29 @@
 import React, { Component } from "react";
+import { CSVLink } from "react-csv";
 
 export default class LocationCollector extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      data: []
     };
   }
 
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(function(position) {
-      console.log("Lat:", position.coords.latitude);
-      console.log("Lon:", position.coords.longitude);
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+    });
+  }
+  headers = [
+    { label: "Latitude", key: "latitude" },
+    { label: "Longitude", key: "longitude" },
+  ];
+
+  updateCoords = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const newList = this.state.data.concat({ latitude: position.coords.latitude, longitude: position.coords.longitude});
+      this.setState({data: newList})
     });
   }
 
@@ -18,6 +31,12 @@ export default class LocationCollector extends Component {
     return (
       <div>
         <h2>GeoLocation</h2>
+        <button onClick={() => this.updateCoords()}>Add Location</button>
+        <br />
+        {this.state.data.map(data => <p>Your latitude is: {data.latitude}, longitude: {data.longitude}</p>)}
+        <CSVLink data={this.state.data} headers={this.headers}>
+          Download CSV
+        </CSVLink>
       </div>
     );
   }

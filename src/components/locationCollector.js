597da2ccdf395c6558f8841434bc6991654ef570
Stuart McClintock <stuart.e.mcc@gmail.com>
2021-12-23 16:08:12 -0800

    first commit

---
+++ src/components/locationCollector.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+export default class LocationCollector extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>GeoLocation</h2>
+      </div>
+    );
+  }
+}
